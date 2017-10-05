a388f29ad56dbf4d1027e83ea70b7302fd21332d
Alexander Ivchenko <A.S.Ivchenko@yandex.ru>
2017-10-05 10:23:43 +0300

    initial commit

---
+++ src/main/java/com/github/alexivchenko/simplewebappwithci/SimpleWebAppWithCiApplication.java
@@ -0,0 +1,12 @@
+package com.github.alexivchenko.simplewebappwithci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleWebAppWithCiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleWebAppWithCiApplication.class, args);
+	}
+}
