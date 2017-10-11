b215f340fe11a7fb9eab1a5b1751addc6a630bff
Alexander Ivchenko <A.S.Ivchenko@yandex.ru>
2017-10-12 02:38:25 +0300

    changed generation id strategy

--- src/main/java/com/github/alexivchenko/simplewebappwithci/model/User.java
+++ src/main/java/com/github/alexivchenko/simplewebappwithci/model/User.java
@@ -5,10 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 /**
  * @author Alex Ivchenko
@@ -20,11 +17,12 @@ import javax.persistence.Table;
 @Table(name = "`user`")
 public @Data class User {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
     private String firstName;
     private String lastName;
     private String email;
+    @Column(name = "`password`")
     private String password;
 }
